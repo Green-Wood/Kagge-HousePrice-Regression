ca9cefb3cdc8352c9b3fbb459f6aef3ab6297000
Green-Wood <1027572886a@gmail.com>
2018-09-10 19:08:43 +0800

    2018.9.9 将optimizer替换为Adam network: 64X64X32 optimizer: Adam Epochs: 700
    Kaggle-loss: 0.15697
    TOP 68%

--- train.py
+++ train.py
@@ -17,16 +17,16 @@ def plot_history(history):
     plt.ylabel('Mean Abs Error [1000$]')
     plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
              label='Train Loss')
-    plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
-             label='Val loss')
+    # plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
+    #          label='Val loss')
     plt.legend()
-    plt.ylim([0, 1])
+    plt.ylim([0, 0.5])
 
 
 train_x = pd.read_csv('./Data/train_x.csv')
 train_y = pd.read_csv('./Data/train_y.csv')
 
-model_name = '5 RMSprop new data'
+model_name = '6 Adam epochs=600'
 
 model = keras.Sequential([
     keras.layers.Dense(64, activation=keras.activations.relu, input_shape=(train_x.shape[1],)),
@@ -35,12 +35,10 @@ model = keras.Sequential([
     keras.layers.Dense(1)
 ])
 
-optimizer = keras.optimizers.RMSprop()
+optimizer = keras.optimizers.Adam()
 model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
 
-early_stop = keras.callbacks.EarlyStopping(monitor='loss', patience=30, min_delta=0.003, mode='min')
-
-history = model.fit(train_x, train_y, epochs=600, validation_split=0.15, verbose=0, callbacks=[PrintDot()])
+history = model.fit(train_x, train_y, epochs=600, verbose=0, callbacks=[PrintDot()])
 model.save('./Model_Analyse/{}.h5'.format(model_name))
 
 plot_history(history)
